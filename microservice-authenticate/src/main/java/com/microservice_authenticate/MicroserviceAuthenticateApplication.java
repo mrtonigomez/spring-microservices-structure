adbf4cabf6255aa0b6a015d41d6427eaf390ff63
Antoni Gomez <tonigomezjuan@gmail.com>
2025-09-23 22:24:10 +0200

    Add auth microservice

---
+++ microservice-authenticate/src/main/java/com/microservice_authenticate/MicroserviceAuthenticateApplication.java
@@ -0,0 +1,13 @@
+package com.microservice_authenticate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAuthenticateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAuthenticateApplication.class, args);
+	}
+
+}
---
+++ microservice-authenticate/src/test/java/com/microservice_authenticate/MicroserviceAuthenticateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice_authenticate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAuthenticateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
